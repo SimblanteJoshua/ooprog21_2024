f64d0521564825e3b288ae72d827ea44882f734a
Joshua G. Simblante <149150550+hiroshi0806@users.noreply.github.com>
2024-08-15 20:18:41 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,16 @@
+public class MovieQuoteInfo {
+    
+    public static void main(String[] args) {
+        
+        String quote = "Sometimes the world tries to knock it out of you. But I believe in music the way some people believe in fairy tales";
+        String movie = "AUGUST RUSH";
+        String character = "Evan Taylor";
+        int year = 2007;
+
+        
+        System.out.println("Quote: \"" + quote + "\"");
+        System.out.println("Movie: " + movie);
+        System.out.println("Character: " + character);
+        System.out.println("Year: " + year);
+    }
+}
