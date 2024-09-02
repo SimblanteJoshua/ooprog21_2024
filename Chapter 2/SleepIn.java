6e86bda84f8440404455cd18845522e86a020e55
Joshua G. Simblante <149150550+SimblanteJoshua@users.noreply.github.com>
2024-09-02 08:46:06 +0800

    Update SleepIn.java

--- Chapter 2/SleepIn.java
+++ Chapter 2/SleepIn.java
@@ -1,11 +1,15 @@
 public class SleepIn {
-  public static boolean sleepIn(boolean weekday, boolean vacation) {
-    return !weekday || vacation;
+  
+  public static void main(String[] args)
+  {
+    System.out.println(sleepIn(false, false));
+    System.out.println(sleepIn(true, false)); 
+    System.out.println(sleepIn(false, true)); 
   }
-
-  public static void main(String[] args) {
-    System.out.println(sleepIn(false, false)); // true
-    System.out.println(sleepIn(true, false)); // false
-    System.out.println(sleepIn(false, true)); // true
-  }
+   
+   public static boolean sleepIn(boolean weekday, boolean vacation)  
+  {
+     
+    return !weekday;
+  } 
 }
