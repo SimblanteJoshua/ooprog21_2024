d4d43c013f7f4ebeb70d0816f75d7a1fc67e5b52
Joshua G. Simblante <149150550+SimblanteJoshua@users.noreply.github.com>
2024-09-16 21:39:01 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,10 @@
+public class FirstAddressExercise {
+  public static void main(String[] args) {
+    String companyName = "Vivo Communication Technology Co. Ltd.";
+    String hqAddress = "Vivo Tower, Zhangzhou, Longgang District, Shenzhen, Guangdong, China";
+
+    System.out.println("XYZ Company");
+    System.out.println(hqAddress);
+    System.out.println("Company: " + companyName);
+  }
+}
